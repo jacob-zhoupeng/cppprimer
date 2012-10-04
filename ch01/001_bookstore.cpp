0ca59c5ea53a83a75475077f94b801e49bba031c
Zhou Peng <peng.zhoupeng@gmail.com>
2012-10-05 01:36:14 +0800

    dir arch

---
+++ ch01/001_bookstore.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void read() {
+	cout << "read()\n";
+}
+
+void sort() {
+	cout << "sort()\n";
+}
+
+void compact() {
+	cout << "compact()\n";
+}
+
+void write() {
+	cout << "write()\n";
+}
+
+int main(void) {
+	read();
+	sort();
+	compact();
+	write();
+
+	return 0;
+}
